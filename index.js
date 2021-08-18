f4acb4394f796005d7921c384690036a78fcb6e9
jny-btk <84020266+jny-btk@users.noreply.github.com>
2021-08-18 11:52:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ new Vue({
     addTaxe: '20',
     active: false,
     show: true,
+    isEditing: false,
+    newTodoDescription: "",
+  },
+  props: {
+    description: String,
+    completed: Boolean
   },
   methods: {
     addTodo() {
@@ -21,17 +27,30 @@ new Vue({
       this.todos.splice(index, 1);
     },
 
-    updateTodo: function(todo) {
-      const updateTodo = { 
-        id: _.findIndex(this.todos, todo),
-        value: this.userInput 
-      };
-      this.todos.splice(updateTodo);
+    // updateTodo: function(todo) {
+    //   const updateTodo = { 
+    //     id: _.findIndex(this.todos, todo),
+    //     value: this.userInput 
+    //   };
+    //   this.todos.splice(updateTodo);
+    // },
+    
+    
+    updateTodo() {
+      if (this.isEditing) {
+        this.finishEditing();
+      } else {
+        this.newTodoDescription = this.description;
+        this.isEditing = true;
+        this.$nextTick(() => this.$refs.newTodo.focus());
+      }
     },
-    
-    
 
-
+    finishEditing() {
+      this.isEditing = false;
+      this.$emit("on-edit", this.newTodoDescription);
+    }
+  
 
     
 
